2f5ba08e7b341e37b56239959f4b2fa737e86a56
Andrew Ellison <acannonellison@gmail.com>
2021-12-29 18:58:02 -0800

    Format apiCall and boilerplate code

--- utils/apiCall.js
+++ utils/apiCall.js
@@ -1,6 +1,7 @@
 // Coinpaprika calls to populate challenges
 
 // Coin lookup
+// Populate data from COIN model
 var coinToID = {
   'Bitcoin (BTC)': 'btc-bitcoin',
   'Ethereum (ETH)': 'eth-ethereum',
@@ -18,15 +19,39 @@ var coinToID = {
 let currentUrl= "https://api.coinpaprika.com/v1/coins/" +coinToID[searchValue] +"/markets";
 
 // Long series of .then calls to make sure that both historical and current price data are passed to the charting functions
-fetch(historicalUrl)
-  .then(function(historicalResponse){
-  return historicalResponse.json()
-  }).then(function(historical){
-    fetch(currentUrl)
-      .then(function(response){
-        return response.json()
-      }).then(function(data){
-        calcAndChart(searchValue,data[0].quotes.USD.price,historical,'coin');
-        getTweet();
-      }) 
-  })
+const singleCall = async (coinID) => {
+  
+  if (cointoID[coinID]) {
+    const coinUrl = `https://api.coinpaprika.com/v1/coins/${coinToID[coinID]}/markets`;
+  } else {
+    return -1
+  }
+
+  try {
+    const response = await fetch(coinUrl);
+    const coinData = response.json();
+
+    return coinData[0].quotes.USD.price
+  } catch (err) {
+    console.log(err);
+    // Pause for a moment and try again 
+    setTimeout(singleCall(coinID),1000);
+
+  }
+  
+
+
+}
+
+// fetch(historicalUrl)
+//   .then(function(historicalResponse){
+//   return historicalResponse.json()
+//   }).then(function(historical){
+//     fetch(currentUrl)
+//       .then(function(response){
+//         return response.json()
+//       }).then(function(data){
+//         calcAndChart(searchValue,data[0].quotes.USD.price,historical,'coin');
+//         getTweet();
+//       }) 
+//   })
