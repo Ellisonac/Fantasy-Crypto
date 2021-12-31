62594a1c202766d13d710af4f07e0025cdfc3628
Dan Beck <danjacobbeck@gmail.com>
2021-12-31 12:24:23 -0800

    commit on 'main'

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {User} = require('../../models')
 
 router.get('/', async (req, res) => {
   try {
@@ -24,6 +25,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.userId = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.post('/login', async (req, res) => {
   try {
