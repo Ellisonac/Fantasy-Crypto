84e59da88f8b44e67208bbaf661921229a1e0d0f
Andrew Ellison <acannonellison@gmail.com>
2022-01-01 13:43:27 -0800

    Add portfolio view route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,119 +1,116 @@
-const { Challenge, Challenge_Coin_Data, Coin, Portfolio, Portfolio_Coin_Entry } = require('../models');
+const {
+  Challenge,
+  Challenge_Coin_Data,
+  Coin,
+  Portfolio,
+  Portfolio_Coin_Entry,
+} = require("../models");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/challenge/', async (req, res) => {
+router.get("/challenge/", async (req, res) => {
   try {
     const challengeData = await Challenge.findAll();
 
-    const challenges = challengeData.map(challenge => challenge.get({ plain: true }))
+    const challenges = challengeData.map((challenge) =>
+      challenge.get({ plain: true })
+    );
 
     res.render("all_challenges", {
-      challenges
-    })
-
+      challenges,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-
 });
 
-router.get('/challenge/:id', async (req, res) => {
+router.get("/challenge/:id", async (req, res) => {
   try {
     const challengeData = await Challenge.findByPk(req.params.id, {
       include: [
         {
-          model: Challenge_Coin_Data
-        }
-      ]
+          model: Challenge_Coin_Data,
+        },
+      ],
     });
 
     const challenge = challengeData.get({ plain: true });
 
     const coinData = await Coin.findAll();
 
-    const coins = coinData.map((coin) =>
-      coin.get({ plain: true })
-    );
+    const coins = coinData.map((coin) => coin.get({ plain: true }));
 
     res.render("challenge_form", {
       challenge,
-      coins
-    })
-
-
-
+      coins,
+    });
   } catch (err) {
     console.log(err);
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-// Get an individual portfolio, 
+// Get an individual portfolio,
 // TODO check if user is correct user
-router.get('/portfolio/:id', async (req, res) => {
 
+// May move functionality to /challenge/:id
+// User may want to see current portfolio + challenge info
+router.get("/portfolio/:id", async (req, res) => {
   try {
-    const portfolioData = await Portfolio.findByPk(req.params.id,{
+    const portfolioData = await Portfolio.findByPk(req.params.id, {
       include: [
         {
-          model: Portfolio_Coin_Entry
+          model: Portfolio_Coin_Entry,
         },
-      ]
+      ],
     });
 
     const portfolio = portfolioData.get({ plain: true });
 
     if (portfolio.user_id != req.session.user_id) {
-      console.log('INVALID USER');
+      console.log("INVALID USER");
     }
 
     const portfolioEntries = portfolio.portfolio_coin_entries;
 
     const coinEntryData = await Challenge_Coin_Data.findAll({
       where: {
-        challenge_id: portfolioData.challenge_id
-      }
+        challenge_id: portfolioData.challenge_id,
+      },
     });
 
-    const coinEntries = coinEntryData.map(coin => coin.get({ plain:true }))
+    const coinEntries = coinEntryData.map((coin) => coin.get({ plain: true }));
 
     const coinData = await Coin.findAll();
 
-    const coinLookup = coinData.map(coin => coin.get({ plain:true }));
-
-    console.log(coinLookup);
+    const coinLookup = coinData.map((coin) => coin.get({ plain: true }));
 
     // Build coins database
-    let coins = []
+    let coins = [];
     for (const entry of portfolioEntries) {
-      
       const amount = parseInt(entry.amount);
-      const coinValues = coinEntries.filter(coin => coin.coin_id == entry.coin_id)[0];
-      console.log(coinValues);
-      console.log(entry.amount,coinValues.start_value);
-      const value = amount*parseFloat(coinValues.start_value);
-      console.log(value);
-      const coin_name = coinLookup[entry.coin_id-1].name;
+      const coinValues = coinEntries.filter(
+        (coin) => coin.coin_id == entry.coin_id
+      )[0];
+      const value = amount * parseFloat(coinValues.start_value);
+      const coin_name = coinLookup[entry.coin_id - 1].name;
       coins.push({
-        id: coinLookup[entry.coin_id-1].id,
+        id: coinLookup[entry.coin_id - 1].id,
         name: coin_name,
         amount,
         value,
       });
     }
 
-    res.render("portfolio",{
+    res.render("portfolio", {
       portfolio: portfolio,
-      coinEntries: coins
-    })
-
+      coinEntries: coins,
+    });
   } catch (err) {
     console.log(err);
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-
-})
+});
 
 module.exports = router;
