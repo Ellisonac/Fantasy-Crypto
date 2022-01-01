fd4877e95ceaa25aee8922e30de8f442af629c4b
Andrew Ellison <acannonellison@gmail.com>
2021-12-31 16:28:47 -0800

    Add all challenges and individual portfolio route.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,23 @@
-const { Challenge, Challenge_Coin_Data, Coin } = require('../models');
+const { Challenge, Challenge_Coin_Data, Coin, Portfolio, Portfolio_Coin_Entry } = require('../models');
 
 const router = require('express').Router();
+
+router.get('/challenge/', async (req, res) => {
+  try {
+    const challengeData = await Challenge.findAll();
+
+    const challenges = challengeData.map(challenge => challenge.get({ plain: true }))
+
+    res.render("all_challenges", {
+      challenges
+    })
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+});
 
 router.get('/challenge/:id', async (req, res) => {
   try {
@@ -32,5 +49,71 @@ router.get('/challenge/:id', async (req, res) => {
     res.status(500).json(err)
   }
 });
+
+// Get an individual portfolio, 
+// TODO check if user is correct user
+router.get('/portfolio/:id', async (req, res) => {
+
+  try {
+    const portfolioData = await Portfolio.findByPk(req.params.id,{
+      include: [
+        {
+          model: Portfolio_Coin_Entry
+        },
+      ]
+    });
+
+    const portfolio = portfolioData.get({ plain: true });
+
+    if (portfolio.user_id != req.session.user_id) {
+      console.log('INVALID USER');
+    }
+
+    const portfolioEntries = portfolio.portfolio_coin_entries;
+
+    const coinEntryData = await Challenge_Coin_Data.findAll({
+      where: {
+        challenge_id: portfolioData.challenge_id
+      }
+    });
+
+    const coinEntries = coinEntryData.map(coin => coin.get({ plain:true }))
+
+    const coinData = await Coin.findAll();
+
+    const coinLookup = coinData.map(coin => coin.get({ plain:true }));
+
+    console.log(coinLookup);
+
+    // Build coins database
+    let coins = []
+    for (const entry of portfolioEntries) {
+      
+      const amount = parseInt(entry.amount);
+      const coinValues = coinEntries.filter(coin => coin.coin_id == entry.coin_id)[0];
+      console.log(coinValues);
+      console.log(entry.amount,coinValues.start_value);
+      const value = amount*parseFloat(coinValues.start_value);
+      console.log(value);
+      const coin_name = coinLookup[entry.coin_id-1].name;
+      coins.push({
+        id: coinLookup[entry.coin_id-1].id,
+        name: coin_name,
+        amount,
+        value,
+      });
+    }
+
+    res.render("portfolio",{
+      portfolio: portfolio,
+      coinEntries: coins
+    })
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err)
+  }
+
+})
 
 module.exports = router;
