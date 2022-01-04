ea7313d72ab307fecd923466a32219118ea3f100
Reginald Capanzana <reggiecapanzana@gmail.com>
2022-01-04 15:29:08 -0800

    Skeleton code for piechart.

---
+++ public/js/piechart.js
@@ -0,0 +1,46 @@
+const createPieChart = async () => {
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+
+    const config = {
+        type: 'pie',
+        data: data,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Pie Chart'
+            }
+          }
+        },
+      };
+
+      var myChart = new Chart(
+        document.getElementById('pie_chart'),
+        config
+      );
+};
+
+if(document.readyState === 'loading'){
+    document
+    .addEventListener('DOMContentLoaded', createPieChart);
+}
