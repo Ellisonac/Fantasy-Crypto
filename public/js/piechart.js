041cf14a6c12661053d2f6ee71a4e277eccd4d0a
Reginald Capanzana <reggiecapanzana@gmail.com>
2022-01-04 18:00:33 -0800

    Data added to chart

--- public/js/piechart.js
+++ public/js/piechart.js
@@ -2,6 +2,7 @@ const createPieChart = async () => {
     const coinsData = document.querySelectorAll("li.card");
 
     let coins = [];
+
     for(let i = 0; i < 3; i++){
         let coinName = coinsData[i].childNodes[1].textContent;
         coinName = coinName.split(" ");
@@ -16,17 +17,15 @@ const createPieChart = async () => {
         coins.push(coin);
     };
 
-    console.log(coins);
-
     const data = {
         labels: [
-          'Red',
-          'Blue',
-          'Yellow'
+          coins[0].name,
+          coins[1].name,
+          coins[2].name
         ],
         datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
+          label: 'Portfolio Breakdown',
+          data: [coins[0].amount, coins[1].amount, coins[2].amount],
           backgroundColor: [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
@@ -41,6 +40,7 @@ const createPieChart = async () => {
         data: data,
         options: {
           responsive: true,
+          radius: 450,
           plugins: {
             legend: {
               position: 'top',
